1b9897c937742c1ed4d4cb4a4b82c6d5c6f86b0e
raghavajag <raghavajagarwal@gmail.com>
2023-11-09 22:48:07 +0530

    fix: return next error, ref code

--- src/controllers/teams/teamController.js
+++ src/controllers/teams/teamController.js
@@ -1,5 +1,6 @@
 const Teams = require("../../models/hackTeams");
 const ErrorResponse = require("../../utils/errorResponse");
+const uuid = require("uuid");
 
 exports.createTeam = async (req, res, next) => {
   const { teamName, projectName } = req.body;
@@ -7,13 +8,16 @@ exports.createTeam = async (req, res, next) => {
   if (team) {
     return next(new ErrorResponse("Team already exists", 400))
   }
+  const referralCode = uuid.v4();
+
   await new Teams({
     teamName,
     projectName,
-    teamLeaderId: req.user._id
+    teamLeaderId: req.user._id,
+    referralCode
   }).save();
 
-  return res.json({ success: true, message: "Team created successfully" })
+  return res.json({ success: true, message: "Team created successfully", data: { code: referralCode } })
 }
 exports.getTeams = async (req, res, next) => {
   const teams = await Teams.find({}).populate('teamLeaderId', 'name email')
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -7,20 +7,20 @@ const auth = async (req, res, next) => {
   const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
-    next(new ErrorResponse("Access Denied: No token provided", 401))
+    return next(new ErrorResponse("Access Denied: No token provided", 401))
   }
 
   try {
     const tokenDetails = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findById({ _id: tokenDetails._id });
     if (!user) {
-      next(new ErrorResponse("Access Denied: Invalid token", 401))
+      return next(new ErrorResponse("Access Denied: Invalid token", 401))
     }
     req.user = { _id: user._id };
     next();
   } catch (error) {
     console.log(error);
-    next(new ErrorResponse("Access Denied: Invalid token", 401))
+    return next(new ErrorResponse("Access Denied: Invalid token", 401))
   }
 };
 
