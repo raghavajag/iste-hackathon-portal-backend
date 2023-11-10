99034cc4cbb2bac0b92a3e31dfc64fa26d775e96
raghavajag <raghavajagarwal@gmail.com>
2023-11-10 08:05:00 +0530

    feat: add, remove members

--- src/controllers/teams/teamController.js
+++ src/controllers/teams/teamController.js
@@ -1,4 +1,4 @@
-const Teams = require("../../models/hackTeams");
+const Teams = require("../../models/teamsModel");
 const ErrorResponse = require("../../utils/errorResponse");
 const uuid = require("uuid");
 
@@ -14,6 +14,7 @@ exports.createTeam = async (req, res, next) => {
     teamName,
     projectName,
     teamLeaderId: req.user._id,
+    members: [req.user._id],
     referralCode
   }).save();
 
@@ -22,4 +23,46 @@ exports.createTeam = async (req, res, next) => {
 exports.getTeams = async (req, res, next) => {
   const teams = await Teams.find({}).populate('teamLeaderId', 'name email')
   return res.json({ success: true, teams })
+}
+exports.addMember = async (req, res, next) => {
+  const { code } = req.body
+  const memberExists = await Teams.findOne({ members: req.user._id })
+  if (memberExists) {
+    return next(new ErrorResponse("User is already a member of some team", 400));
+  }
+  const team = await Teams.findOne({ referralCode: code })
+  if (!team) {
+    return next(new ErrorResponse("Invalid referral code", 400))
+  }
+  if (team.members.length >= 4) {
+    return next(new ErrorResponse("Team is full", 400))
+  }
+  team.members.push(req.user._id)
+  await team.save();
+
+  return res.json({ success: true, message: "Member added to the team successfully" });
+}
+exports.removeMember = async (req, res, next) => {
+  const { teamId, memberId } = req.params;
+  const team = await Teams.findById(teamId);
+
+  if (!team) {
+    return next(new ErrorResponse("Team not found", 404));
+  }
+
+  if (team.teamLeaderId.toString() !== req.user._id.toString()) {
+    return next(new ErrorResponse("You don't have permission to remove a member", 403));
+  }
+  if (team.teamLeaderId.toString() === memberId) {
+    return next(new ErrorResponse("Team leader cannot be removed", 403));
+  }
+  if (!team.members.includes(memberId)) {
+    return next(new ErrorResponse("Member not found in the team", 404));
+  }
+
+  team.members = team.members.filter((member) => member.toString() !== memberId);
+
+  await team.save();
+
+  return res.json({ success: true, message: "Member removed from the team successfully" });
 }
--- src/routes/teamRoutes.js
+++ src/routes/teamRoutes.js
@@ -6,4 +6,6 @@ const auth = require('../middleware/authMiddleware')
 
 teamRouter.post("/", auth, asyncHandler(teamController.createTeam));
 teamRouter.get("/", auth, asyncHandler(teamController.getTeams));
+teamRouter.post("/members", auth, asyncHandler(teamController.addMember));
+teamRouter.delete("/members/:memberId/:teamId", auth, asyncHandler(teamController.removeMember));
 module.exports = teamRouter;
